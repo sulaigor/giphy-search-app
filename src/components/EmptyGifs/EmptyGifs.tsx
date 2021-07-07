4887eee371b8d5184ed64099c16a568cf88f7fbb
Igor Šula <sulaigor@gmail.com>
2021-07-08 00:14:46 +0200

    Add empty gifs component

---
+++ src/components/EmptyGifs/EmptyGifs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmptyGifs = () => {
+  return (
+    <div>
+      <h2>No Gifs...</h2>
+      <p>Try to find some!</p>
+    </div>
+  );
+};
+
+export default EmptyGifs;
