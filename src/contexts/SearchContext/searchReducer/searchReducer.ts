3a929298af47871fa3852e1cb34ef8a28e1259c3
Igor Šula <sulaigor@gmail.com>
2021-07-07 23:54:31 +0200

    Add set search query case in search readucer

--- src/contexts/SearchContext/searchReducer/searchReducer.ts
+++ src/contexts/SearchContext/searchReducer/searchReducer.ts
@@ -1,10 +1,17 @@
 import { ReducerFunctionType } from 'types/reducer';
+import { SET_SEARCH_QUERY } from './actionTypes';
 import { IReducerState } from './types';
 
 const searchReducer: ReducerFunctionType<IReducerState> = (state, action) => {
-  const { type } = action;
+  const { type, payload } = action;
 
   switch (type) {
+    case SET_SEARCH_QUERY:
+      return {
+        ...state,
+        searchQuery: payload?.newSearchQuery as string,
+      };
+
     default:
       return state;
   }
