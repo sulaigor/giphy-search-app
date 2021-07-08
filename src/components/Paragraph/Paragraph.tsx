d01ae46651c00d21adeda2e3c64b2a2dc2b19072
Igor Šula <sulaigor@gmail.com>
2021-07-09 00:54:30 +0200

    Edit look of paragraph component

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,7 +1,8 @@
 import { IStringChildrenProps as IProps } from 'types/props';
+import css from './paragraph.module.scss';
 
 const Paragraph = ({ children }: IProps) => {
-  return <p>{children}</p>;
+  return <p className={css.paragraph}>{children}</p>;
 };
 
 export default Paragraph;
