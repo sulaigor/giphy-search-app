c47be43bb30d37a18cbf12f3997417da9e835f5b
Igor Šula <sulaigor@gmail.com>
2021-07-09 00:27:21 +0200

    Add sumorize of max pages value in search context utils

--- src/contexts/SearchContext/utils.ts
+++ src/contexts/SearchContext/utils.ts
@@ -1,28 +1,30 @@
 import axios from 'axios';
 import queryString from 'query-string';
-import { PAGINATION_ITEMS_COUNT } from 'const/pagination';
+import { MAX_PAGES_COUNT, PAGINATION_ITEMS_COUNT } from 'const/pagination';
 import { IGif } from 'types/gifs';
 import { getEnviroment } from 'utils/enviroment';
 
-const getGifsOffset = (page: number) => Math.min(page - 1, 0) * PAGINATION_ITEMS_COUNT;
+const getGifsOffset = (page: number) => Math.max(page - 1, 0) * PAGINATION_ITEMS_COUNT;
+const getMaxPagesCount = (gifsTotalCount: number): number =>
+  Math.min(Math.floor(gifsTotalCount / PAGINATION_ITEMS_COUNT), MAX_PAGES_COUNT);
 
-const getSearchQuery = (searchQuery: string, page: number): Record<string, string | number> => {
+const getSearchQuery = (searchQuery: string, currentPage: number): Record<string, string | number> => {
   const { REACT_APP_API_KEY } = getEnviroment();
 
   return {
     api_key: REACT_APP_API_KEY,
     q: searchQuery,
     limit: PAGINATION_ITEMS_COUNT,
-    offset: getGifsOffset(page),
+    offset: getGifsOffset(currentPage),
   };
 };
 
-const getSearchUrl = (searchQuery: string, page: number): string => {
+const getSearchUrl = (searchQuery: string, currentPage: number): string => {
   const { REACT_APP_SEARCH_ENDPOINT } = getEnviroment();
 
   return queryString.stringifyUrl({
     url: REACT_APP_SEARCH_ENDPOINT,
-    query: getSearchQuery(searchQuery, page),
+    query: getSearchQuery(searchQuery, currentPage),
   });
 };
 
@@ -31,12 +33,19 @@ interface IGetGifsReturn {
   maxPages: number;
 }
 
-export const getGifs = async (searchQuery: string, page: number): Promise<IGetGifsReturn> => {
+interface IGifsResponseData {
+  data: IGif[];
+  pagination: {
+    total_count: number;
+  };
+}
+
+export const getGifs = async (searchQuery: string, currentPage: number): Promise<IGetGifsReturn> => {
+  const { data: responseData } = await axios.get<IGifsResponseData>(getSearchUrl(searchQuery, currentPage));
   const {
-    data: { data: newGifs },
-  } = await axios.get<{ data: IGif[] }>(getSearchUrl(searchQuery, page));
+    data: newGifs,
+    pagination: { total_count: totalCount },
+  } = responseData;
 
-  const maxPages = 10;
-
-  return { newGifs, maxPages };
+  return { newGifs, maxPages: getMaxPagesCount(totalCount) };
 };
