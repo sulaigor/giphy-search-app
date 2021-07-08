f1fe37bedef76fb533c93a9e8f5dbabeab371b08
Igor Šula <sulaigor@gmail.com>
2021-07-09 01:04:37 +0200

    Add classname property to paragraph component

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,8 +1,13 @@
-import { IStringChildrenProps as IProps } from 'types/props';
+import classNames from 'classnames';
+import { IStringChildrenProps } from 'types/props';
 import css from './paragraph.module.scss';
 
-const Paragraph = ({ children }: IProps) => {
-  return <p className={css.paragraph}>{children}</p>;
+interface IProps extends IStringChildrenProps {
+  className?: string;
+}
+
+const Paragraph = ({ children, className }: IProps) => {
+  return <p className={classNames(css.paragraph, className)}>{children}</p>;
 };
 
 export default Paragraph;
