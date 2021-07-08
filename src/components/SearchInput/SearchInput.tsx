07eb6ad51fe41cf8090d65f0838f983480c30951
Igor Šula <sulaigor@gmail.com>
2021-07-09 01:48:42 +0200

    Edit look of search input component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent } from 'react';
 import Button from 'components/Button';
 import { useSearchContext } from 'contexts/SearchContext';
+import css from './searchInput.module.scss';
 
 const SearchInput = () => {
   const { searchQuery, setSearchQuery } = useSearchContext();
@@ -8,10 +9,10 @@ const SearchInput = () => {
   const handleSearchInputChange = ({ target }: ChangeEvent<HTMLInputElement>) => setSearchQuery(target.value);
 
   return (
-    <div>
-      <input type='text' value={searchQuery} onChange={handleSearchInputChange} />
-      <div>
-        <Button onClick={() => {}}>Search</Button>
+    <div className={css.wrapper}>
+      <input type='text' className={css.searchInput} value={searchQuery} onChange={handleSearchInputChange} />
+      <div className={css.actionsWrapper}>
+        <Button onClick={() => {}}>Clear</Button>
       </div>
     </div>
   );
