c7b9aa9c9e6ada3cea69c56819644cb13de20355
Igor Šula <sulaigor@gmail.com>
2021-07-08 21:33:41 +0200

    Add downloading of gifs in search context component

--- src/contexts/SearchContext/SearchContext.tsx
+++ src/contexts/SearchContext/SearchContext.tsx
@@ -1,9 +1,14 @@
 import { createContext, useContext, useReducer } from 'react';
+import axios from 'axios';
+import { GifType } from 'types/gifs';
 import { IChildrenProps as IProps } from 'types/props';
+import { DEFAULT_DEBOUNCE_DURATION } from 'const/debounce';
+import { useDebounce } from 'hooks/useDebounce';
 import { dummyFunction } from 'utils/dummy';
 import searchReducer, { initialState } from './searchReducer';
-import { setSearchQueryAction } from './searchReducer/actions';
+import { setGifsAction, setSearchQueryAction } from './searchReducer/actions';
 import { SearchContextType } from './types';
+import { getSearchUrl } from './utils';
 
 const Context = createContext<SearchContextType>({
   ...initialState,
@@ -12,7 +17,27 @@ const Context = createContext<SearchContextType>({
 
 const SearchContext = ({ children }: IProps) => {
   const [reducerState, dispatch] = useReducer(searchReducer, initialState);
+  const { searchQuery } = reducerState;
 
+  useDebounce(
+    async () => {
+      if (!searchQuery) return;
+
+      try {
+        const {
+          data: { data: newGifs },
+        } = await axios.get<{ data: GifType[] }>(getSearchUrl(searchQuery));
+
+        if (newGifs.length > 0) setGifs(newGifs);
+      } catch (err) {
+        console.error('Get new gifs error:', err);
+      }
+    },
+    [searchQuery],
+    DEFAULT_DEBOUNCE_DURATION
+  );
+
+  const setGifs = (newGifs: GifType[]) => dispatch(setGifsAction(newGifs));
   const setSearchQuery = (newSearchQuery: string) => dispatch(setSearchQueryAction(newSearchQuery));
 
   const contextValue: SearchContextType = {
