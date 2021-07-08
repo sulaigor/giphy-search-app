69d79c7e6389a7b4d3dd5196164dcdfb7bb96947
Igor Šula <sulaigor@gmail.com>
2021-07-08 21:30:20 +0200

    Add set gifs action in search reducer

--- src/contexts/SearchContext/searchReducer/actionTypes.ts
+++ src/contexts/SearchContext/searchReducer/actionTypes.ts
@@ -1,1 +1,3 @@
 export const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
+
+export const SET_GIFS = 'SET_GIFS';
--- src/contexts/SearchContext/searchReducer/actions.ts
+++ src/contexts/SearchContext/searchReducer/actions.ts
@@ -1,7 +1,13 @@
+import { GifType } from 'types/gifs';
 import { IReducerAction } from 'types/reducer';
-import { SET_SEARCH_QUERY } from './actionTypes';
+import { SET_GIFS, SET_SEARCH_QUERY } from './actionTypes';
 
 export const setSearchQueryAction = (newSearchQuery: string): IReducerAction => ({
   type: SET_SEARCH_QUERY,
   payload: { newSearchQuery },
+});
+
+export const setGifsAction = (newGifs: GifType[]): IReducerAction => ({
+  type: SET_GIFS,
+  payload: { newGifs },
 });
