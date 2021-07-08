f3e63eeab26edca3057ac2c1dd47533645b56c69
Igor Šula <sulaigor@gmail.com>
2021-07-08 22:55:15 +0200

    Add set page action to search reducer

--- src/contexts/SearchContext/searchReducer/actionTypes.ts
+++ src/contexts/SearchContext/searchReducer/actionTypes.ts
@@ -1,3 +1,5 @@
 export const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
 
+export const SET_PAGE = 'SET_PAGE';
+
 export const SET_GIFS = 'SET_GIFS';
--- src/contexts/SearchContext/searchReducer/actions.ts
+++ src/contexts/SearchContext/searchReducer/actions.ts
@@ -1,10 +1,15 @@
 import { IGif } from 'types/gifs';
 import { IReducerAction } from 'types/reducer';
-import { SET_GIFS, SET_SEARCH_QUERY } from './actionTypes';
+import { SET_GIFS, SET_PAGE, SET_SEARCH_QUERY } from './actionTypes';
 
 export const setSearchQueryAction = (newSearchQuery: string): IReducerAction => ({
   type: SET_SEARCH_QUERY,
   payload: { newSearchQuery },
+});
+
+export const setPageAction = (newPage: number): IReducerAction => ({
+  type: SET_PAGE,
+  payload: { newPage },
 });
 
 export const setGifsAction = (newGifs: IGif[]): IReducerAction => ({
