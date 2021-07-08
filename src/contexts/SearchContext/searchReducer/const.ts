1673e92b81fe608fc7ed23fb035a05a975ccaf4e
Igor Šula <sulaigor@gmail.com>
2021-07-08 22:33:42 +0200

    Add number of page to search reducer

--- src/contexts/SearchContext/searchReducer/const.ts
+++ src/contexts/SearchContext/searchReducer/const.ts
@@ -2,5 +2,6 @@ import { IReducerState } from './types';
 
 export const initialState: IReducerState = {
   searchQuery: '',
+  page: 1,
   gifs: null,
 };
--- src/contexts/SearchContext/searchReducer/types.ts
+++ src/contexts/SearchContext/searchReducer/types.ts
@@ -2,5 +2,6 @@ import { IGif } from 'types/gifs';
 
 export interface IReducerState {
   searchQuery: string;
+  page: number;
   gifs: IGif[] | null;
 }
