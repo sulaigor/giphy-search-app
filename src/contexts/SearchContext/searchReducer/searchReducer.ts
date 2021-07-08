f5b03602a69a289086437b32d7fa220f44d816cc
Igor Šula <sulaigor@gmail.com>
2021-07-08 23:44:56 +0200

    Add set max pages case in search reducer function

--- src/contexts/SearchContext/searchReducer/searchReducer.ts
+++ src/contexts/SearchContext/searchReducer/searchReducer.ts
@@ -1,6 +1,6 @@
 import { IGif } from 'types/gifs';
 import { ReducerFunctionType } from 'types/reducer';
-import { SET_GIFS, SET_PAGE, SET_SEARCH_QUERY } from './actionTypes';
+import { SET_GIFS, SET_MAX_PAGE, SET_PAGE, SET_SEARCH_QUERY } from './actionTypes';
 import { IReducerState } from './types';
 
 const searchReducer: ReducerFunctionType<IReducerState> = (state, action) => {
@@ -17,6 +17,12 @@ const searchReducer: ReducerFunctionType<IReducerState> = (state, action) => {
       return {
         ...state,
         page: Math.max(payload?.newPage as number, 0),
+      };
+
+    case SET_MAX_PAGE:
+      return {
+        ...state,
+        maxPages: Math.max(state.page, payload?.newMaxPages as number),
       };
 
     case SET_GIFS:
