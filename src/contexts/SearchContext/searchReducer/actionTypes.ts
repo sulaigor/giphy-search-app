4faec254cf8dc7eabcfa24af0dfdc66cdb3dce4f
Igor Šula <sulaigor@gmail.com>
2021-07-08 23:44:24 +0200

    Add set max pages action in search reducer

--- src/contexts/SearchContext/searchReducer/actionTypes.ts
+++ src/contexts/SearchContext/searchReducer/actionTypes.ts
@@ -1,5 +1,6 @@
 export const SET_SEARCH_QUERY = 'SET_SEARCH_QUERY';
 
 export const SET_PAGE = 'SET_PAGE';
+export const SET_MAX_PAGE = 'SET_MAX_PAGE';
 
 export const SET_GIFS = 'SET_GIFS';
--- src/contexts/SearchContext/searchReducer/actions.ts
+++ src/contexts/SearchContext/searchReducer/actions.ts
@@ -1,6 +1,6 @@
 import { IGif } from 'types/gifs';
 import { IReducerAction } from 'types/reducer';
-import { SET_GIFS, SET_PAGE, SET_SEARCH_QUERY } from './actionTypes';
+import { SET_GIFS, SET_MAX_PAGE, SET_PAGE, SET_SEARCH_QUERY } from './actionTypes';
 
 export const setSearchQueryAction = (newSearchQuery: string): IReducerAction => ({
   type: SET_SEARCH_QUERY,
@@ -10,6 +10,11 @@ export const setSearchQueryAction = (newSearchQuery: string): IReducerAction => 
 export const setPageAction = (newPage: number): IReducerAction => ({
   type: SET_PAGE,
   payload: { newPage },
+});
+
+export const setMaxPagesAction = (newMaxPages: number): IReducerAction => ({
+  type: SET_MAX_PAGE,
+  payload: { newMaxPages },
 });
 
 export const setGifsAction = (newGifs: IGif[]): IReducerAction => ({
