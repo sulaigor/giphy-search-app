079ed0179bd0175285982ce629982631ce3cee75
Igor Šula <sulaigor@gmail.com>
2021-07-08 23:26:55 +0200

    Add type property to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react';
 
+enum BUTTON_TYPE {
+  primary = 'primary',
+  link = 'link',
+}
+
 interface IProps {
   children: string | ReactNode;
+  type?: keyof typeof BUTTON_TYPE;
   onClick: () => void;
 }
 
-const Button = ({ children, onClick }: IProps) => {
+const Button = ({ children, type = BUTTON_TYPE.link, onClick }: IProps) => {
   return (
     <button type='button' onClick={onClick}>
       {children}
